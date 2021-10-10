d5abb3eec67e84441dc4c290eb92c4d379b522aa
Neferionn <anonymperson@mail.ru>
2021-10-10 13:26:20 +0300

    changed file name

--- taskWithTS.js
+++ taskWithTS.js
@@ -1,46 +1,43 @@
 "use strict";
 class HtmlElement {
-    constructor() { }
-    click() {
-        console.log("CLICK");
-    }
-    focus() {
-        console.log("FOCUS");
-    }
+  constructor() {}
+  click() {
+    console.log("CLICK");
+  }
+  focus() {
+    console.log("FOCUS");
+  }
 }
 const e = new HtmlElement();
 e.click();
 e.focus();
 class HtmlSelectElement extends HtmlElement {
-    constructor(items) {
-        super();
-        this.items = items;
-        if (!items) {
-            console.log(`array empty`);
-            this.items = [];
-        }
-        else {
-            this.items = items;
-        }
+  constructor(items) {
+    super();
+    this.items = items;
+    if (!items) {
+      console.log(`array empty`);
+      this.items = [];
+    } else {
+      this.items = items;
     }
-    showItems() {
-        return this.items;
+  }
+  showItems() {
+    return this.items;
+  }
+  addItem(item) {
+    this.items.push(item);
+  }
+  removeItem(item) {
+    if (this.items.length && this.items.includes(item)) {
+      let itemIndex = this.items.indexOf(item);
+      if (itemIndex !== -1) this.items.splice(itemIndex, 1);
+    } else {
+      throw new Error("Name of element not found");
     }
-    addItem(item) {
-        this.items.push(item);
-    }
-    removeItem(item) {
-        if (this.items.length && this.items.includes(item)) {
-            let itemIndex = this.items.indexOf(item);
-            if (itemIndex !== -1)
-                this.items.splice(itemIndex, 1);
-        }
-        else {
-            throw new Error("Name of element not found");
-        }
-        console.log(`Removed "${item}" from array`);
-        return this.items;
-    }
+    console.log(`Removed "${item}" from array`);
+    return this.items;
+  }
 }
 const arr = ["div", "span", "article", "main", "footer", "nav"];
 const selectElement = new HtmlSelectElement(arr);
@@ -51,7 +48,7 @@ console.log(selectElement.removeItem("main"));
 console.log(selectElement.showItems());
 selectElement.focus();
 selectElement.click();
-const emptyElement = new HtmlSelectElement([]);
+const emptyElement = new HtmlSelectElement();
 console.log(emptyElement.showItems());
 emptyElement.addItem("customTag");
 console.log(emptyElement.showItems());
